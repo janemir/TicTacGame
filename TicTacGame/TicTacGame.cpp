600543123bb212b41d3d041fc8d0418a56a6e08f
janemir <144587149+janemir@users.noreply.github.com>
2023-10-09 14:46:48 +0300

    checkWin
    написала ф-ю проверки победителя

--- TicTacGame/TicTacGame.cpp
+++ TicTacGame/TicTacGame.cpp
@@ -25,6 +25,25 @@ void printBoard(const char board[3][3]) {
         }
     }
 }
+//Проверка победителя Тask 12
+bool checkWin(const char board[3][3], char currentPlayer) {
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+            return true;
+        }
+        if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+            return true;
+        }
+    }
+    if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+        return true;
+    }
+    if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+        return true;
+    }
+    return false;
+}
+
 //Крестик или нолик Task 11
 void playGame() {
     char board[3][3];
